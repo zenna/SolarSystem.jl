797e9b7c507d27f7af0736e4406df5a33e6de5a3
Zenna Tavares <zennatavares@gmail.com>
2021-03-07 21:53:00 -0500

    gj

--- src/MakieExample.jl
+++ src/MakieExample.jl
@@ -4,6 +4,15 @@
 using Markdown
 using InteractiveUtils
 
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
 # ╔═╡ de698868-7f9b-11eb-16af-031849e1f8ac
 using Revise
 
@@ -16,8 +25,11 @@ begin
 	Page(exportable=true, offline=true)
 end 
 
-# ╔═╡ 03c41ab0-7f9e-11eb-2b3e-435a5db42212
-using UnitfulAstro
+# ╔═╡ 57856634-7fad-11eb-3036-4d6a958e57a4
+using PlutoUI
+
+# ╔═╡ 89c59ea0-7faf-11eb-294c-41e589b29f49
+using Observables
 
 # ╔═╡ c61b7d78-7fa2-11eb-3317-27c52adcacd6
 
@@ -54,9 +66,6 @@ mercury = Body(2439.7,0.1,69.059e6)
 # ╔═╡ bcabefb0-7fa8-11eb-220b-4fed094e221f
 venus = Body(6051.8, 0.2, 108.87e6)
 
-# ╔═╡ 8effea2c-7f9d-11eb-0434-99749285b842
-earth = Body(6.36e3, 0.3, 148.41e6)
-
 # ╔═╡ 9237e558-7f9f-11eb-3fd2-9be5c7ebf90d
 mars = Body(3389.4, 0.4, 239.41e6)
 
@@ -76,7 +85,7 @@ neptune = Body(24622, 0.8, 4.4757e9)
 pluto = Body(1188.3, 0.9, 5.9e9)
 
 # ╔═╡ 924bbaa0-7fa5-11eb-3396-1972470784d5
-sun = Body(696340, 0, 0)
+sun = Body(6963.40, 0, 0)
 
 # ╔═╡ 0013cbb0-7fa9-11eb-0bdb-4f172162bf7b
 
@@ -95,6 +104,15 @@ function plot_solar_system(bodies)
 	scene
 end
 
+# ╔═╡ be0c46f0-7fad-11eb-1bae-7d99544cc38c
+# bodies = [sun, earth, venus]
+
+# ╔═╡ 6e8f3936-7fad-11eb-0d65-07026d17bfff
+@bind earth_years PlutoUI.Slider(1:365) 
+
+# ╔═╡ 8effea2c-7f9d-11eb-0434-99749285b842
+earth = Body(6.36e3, earth_years/365, 148.41e6)
+
 # ╔═╡ c3ca09d8-7fa0-11eb-17f8-cf753ad27443
 bodies = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
 
@@ -104,12 +122,70 @@ plot_solar_system(bodies)
 # ╔═╡ a1cf8518-7fa2-11eb-1783-df1ff91cda12
 md"Example"
 
+# ╔═╡ 958f8590-7fad-11eb-1bdb-5f014b47fed4
+
+
+# ╔═╡ ddd21fd8-7fae-11eb-17b1-29c5619a38b7
+begin
+	hue_slider = JSServe.Slider(LinRange(1, 360, 100))
+	color = map(hue_slider) do hue
+		HSV(hue, 0.5, 0.5)
+	end
+	JSServe.with_session() do s, r
+		return DOM.div(hue_slider, color)
+	end
+end
+
+# ╔═╡ 91a21d94-7faf-11eb-152e-27a5d44edf9f
+App() do session::Session
+    index_slider = JSServe.Slider(1:365)
+    # fig = Figure()
+    # ax, cplot = contour(fig[1, 1], volume)
+    # heatmap(fig[1, 2], slice)
+	earth_years = Node(100)
+	#xbodies = @lift 
+	# scene = @lift plot_solar_system([sun, Body(6.36e3, $earth_years/365, 148.41e6)])
+	scene = Scene()
+	for body in bodies
+		@show "hi"
+		body_ = @lift Body(body.radius, $earth_years / 365, body.dist_to_sun)
+		@show "hii"
+		#(x, y) = @lift abs_pos($body_)
+		phase_2pi = @lift $body_.phase * 2pi
+        yx = @lift $body_.dist_to_sun .* sincos($phase_2pi)
+
+		@show "hiii"
+		s = @lift Sphere(Point3f0($yx[2], $yx[1], 0), body.radius*1000)
+		@show "hiiii"
+		mesh!(scene, s, color=RGBf0(1, 0.7, 0.3))
+	end
+	# scene
+	# x = 1.5
+	# y = 5.8
+	# xy = [x y]
+	# res = scatter!(scene, xy)
+	on(index_slider) do idx
+		ey = idx/365
+		@show idx
+		earth_years[] = idx
+		# xbodies[1].phase = ey
+		# x_ = ey
+		# y_ = ey
+		# translate!(res, idx, idx, idx)
+		# xy .= [x_ y_]
+	end
+	slider = DOM.div("z-index: ", index_slider, index_slider.value)
+    return JSServe.record_states(session, DOM.div(slider, scene))
+end
+
+# ╔═╡ 64904030-7fb5-11eb-1633-cf965f8bb5ac
+
+
 # ╔═╡ Cell order:
 # ╠═de698868-7f9b-11eb-16af-031849e1f8ac
 # ╠═e6714650-7f95-11eb-131f-59c3d5f1919f
 # ╠═c61b7d78-7fa2-11eb-3317-27c52adcacd6
 # ╠═d8e66710-7fa2-11eb-108a-9b4940798246
-# ╠═03c41ab0-7f9e-11eb-2b3e-435a5db42212
 # ╠═dfc383ca-7f9c-11eb-1cae-85de56d9cfb3
 # ╠═e9f7f20e-7f9c-11eb-3ab2-37fa0ecc4d97
 # ╠═a3bcff66-7f9d-11eb-3264-91ba931fd331
@@ -130,5 +206,13 @@ md"Example"
 # ╠═934005b0-7fa5-11eb-2bd9-19b44bfb001f
 # ╠═96a2f21c-7fa0-11eb-3ae8-8563b736b30e
 # ╠═c3ca09d8-7fa0-11eb-17f8-cf753ad27443
+# ╠═be0c46f0-7fad-11eb-1bae-7d99544cc38c
 # ╠═642873f2-7fa2-11eb-0756-55cbf93d273d
+# ╠═57856634-7fad-11eb-3036-4d6a958e57a4
+# ╠═6e8f3936-7fad-11eb-0d65-07026d17bfff
 # ╠═a1cf8518-7fa2-11eb-1783-df1ff91cda12
+# ╠═958f8590-7fad-11eb-1bdb-5f014b47fed4
+# ╠═ddd21fd8-7fae-11eb-17b1-29c5619a38b7
+# ╠═89c59ea0-7faf-11eb-294c-41e589b29f49
+# ╠═91a21d94-7faf-11eb-152e-27a5d44edf9f
+# ╠═64904030-7fb5-11eb-1633-cf965f8bb5ac
