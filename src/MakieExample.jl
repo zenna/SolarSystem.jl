b2b8f88789e28b9d59e07480f2b9c029fa96ff33
Zenna Tavares <zennatavares@gmail.com>
2021-03-07 19:20:39 -0500

    spheres

--- src/MakieExample.jl
+++ src/MakieExample.jl
@@ -4,99 +4,100 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ de698868-7f9b-11eb-16af-031849e1f8ac
+using Revise
+
 # ╔═╡ e6714650-7f95-11eb-131f-59c3d5f1919f
 using GLMakie, AbstractPlotting, Colors, FileIO
 
-# ╔═╡ 5b3f6550-7f98-11eb-064d-fb19e99a1ea9
+# ╔═╡ d8e66710-7fa2-11eb-108a-9b4940798246
 begin
 	using JSServe
 	Page(exportable=true, offline=true)
+end 
+
+# ╔═╡ 03c41ab0-7f9e-11eb-2b3e-435a5db42212
+using UnitfulAstro
+
+# ╔═╡ c61b7d78-7fa2-11eb-3317-27c52adcacd6
+
+
+# ╔═╡ dfc383ca-7f9c-11eb-1cae-85de56d9cfb3
+md"# A Newtonion Universe"
+
+# ╔═╡ e9f7f20e-7f9c-11eb-3ab2-37fa0ecc4d97
+Position = Point3f0
+
+# ╔═╡ a3bcff66-7f9d-11eb-3264-91ba931fd331
+Vec2
+
+# ╔═╡ 4aa60eb0-7f9d-11eb-24c7-bd6a81c79b66
+struct Body
+  radius::Float64
+  phase::Float64
+  dist_to_sun::Float64
 end
 
-# ╔═╡ 900e00a0-7f9a-11eb-24ee-1d161eec7d24
-import Pkg; Pkg.add("FileIO")
-
-# ╔═╡ 82d461e0-7f9a-11eb-026b-73f04fa3074d
-catmesh = FileIO.load(GLMakie.assetpath("cat.obj"))
-
-# ╔═╡ 8782a7b0-7f9a-11eb-0395-d59bc1a19187
-gold = FileIO.load(download("https://raw.githubusercontent.com/nidorx/matcaps/master/1024/E6BF3C_5A4719_977726_FCFC82.png"))
-
-# ╔═╡ 8782f5d0-7f9a-11eb-1fc5-c78f4f32650d
-mesh(catmesh, matcap=gold, shading=false)
-
-# ╔═╡ 4db8cd20-7f9a-11eb-21d2-f91970955fb9
-GLMakie.enable_SSAO[] = true
-
-# ╔═╡ b4e7d1a0-7f98-11eb-167c-49f9786f6d56
-N = 60
-
-# ╔═╡ 03baf500-7f99-11eb-2c5f-051c7cb1d6bd
-function xy_data(x, y)
-    r = sqrt(x^2 + y^2)
-    r == 0.0 ? 1f0 : (sin(r)/r)
+# ╔═╡ acdd3c10-7f9e-11eb-15a1-fd2ee4f552c4
+function abs_pos(dist_to_sun, phase)
+  phase_2pi = phase * 2pi
+  y, x = dist_to_sun .* sincos(phase_2pi)
+  return (x, y)
 end
 
-# ╔═╡ 03bb4322-7f99-11eb-044b-dd69e68ab1b1
-l = range(-10, stop = 10, length = N)
+# ╔═╡ be4e263a-7f9e-11eb-3ac1-e791d0b6f87e
+abs_pos(body::Body) = abs_pos(body.dist_to_sun, body.phase)
 
-# ╔═╡ 03bbb850-7f99-11eb-3407-812291e20300
-z = Float32[xy_data(x, y) for x in l, y in l]
+# ╔═╡ 8effea2c-7f9d-11eb-0434-99749285b842
+earth = Body(6.36e3, 0.2, 148.41e6)
 
-# ╔═╡ 03c46ae0-7f99-11eb-1e1b-07a8e974a677
-surface(
-    -1..1, -1..1, z,
-    colormap = :Spectral
-)
+# ╔═╡ 9237e558-7f9f-11eb-3fd2-9be5c7ebf90d
+mars = Body(3389.4, 0.6, 239.41e6)
 
-# ╔═╡ 618a75c0-7f99-11eb-152d-b9425d936d94
-# Alternatively:
-# fig = Figure()
-# scene = LScene(fig[1, 1], scenekw = (SSAO = (radius = 5.0, blur = 3), show_axis=false, camera=cam3d!))
-# scene.scene[:SSAO][:bias][] = 0.025
+# ╔═╡ 1b395122-7fa3-11eb-23bb-cf9a29cd27d2
+scene = Scene()
 
-scene = Scene(show_axis = false)
+# ╔═╡ 96a2f21c-7fa0-11eb-3ae8-8563b736b30e
+function plot_solar_system(bodies, scene = Scene())
+   for body in bodies
+		(x, y) = abs_pos(body)
+		s = Sphere(Point3f0(x, y, 0), body.radius*100)
+		@show body
+		println("")
+		scene = mesh!(scene, s, color=RGBf0(1, 0.7, 0.3))
+	end
+	scene
+end
 
-# ╔═╡ 618fccee-7f99-11eb-3a5a-099361bc502a
-# SSAO attributes are per scene
-scene[:SSAO][:radius][] = 5.0
+# ╔═╡ c3ca09d8-7fa0-11eb-17f8-cf753ad27443
+bodies = [mars, earth]
 
-# ╔═╡ 6197e340-7f99-11eb-2d50-4f4830647f10
-scene[:SSAO][:blur][] = 3
+# ╔═╡ 642873f2-7fa2-11eb-0756-55cbf93d273d
+mesh = plot_solar_system(bodies, scene)
 
-# ╔═╡ 619fab70-7f99-11eb-20ba-a18c27145101
-scene[:SSAO][:bias][] = 0.025
-
-# ╔═╡ 61a79ab0-7f99-11eb-0384-855140c82865
-box = Rect3D(Point3f0(-0.5), Vec3f0(1))
-
-# ╔═╡ 61b0c270-7f99-11eb-3be1-b7cdf8819f78
-positions = [Point3f0(x, y, rand()) for x in -5:5 for y in -5:5]
-
-# ╔═╡ 61b9c320-7f99-11eb-0ab4-810f4d4ba951
-meshscatter!(scene, positions, marker=box, markersize=1, color=:lightblue, ssao=true)
-
-# ╔═╡ 61c2eae0-7f99-11eb-20d5-5d9d94a6bbe2
+# ╔═╡ 7ddc0ba6-7fa2-11eb-1133-c91b4245a1eb
 scene
 
+# ╔═╡ a1cf8518-7fa2-11eb-1783-df1ff91cda12
+md"Example"
+
 # ╔═╡ Cell order:
+# ╠═de698868-7f9b-11eb-16af-031849e1f8ac
 # ╠═e6714650-7f95-11eb-131f-59c3d5f1919f
-# ╠═900e00a0-7f9a-11eb-24ee-1d161eec7d24
-# ╠═5b3f6550-7f98-11eb-064d-fb19e99a1ea9
-# ╠═82d461e0-7f9a-11eb-026b-73f04fa3074d
-# ╠═8782a7b0-7f9a-11eb-0395-d59bc1a19187
-# ╠═8782f5d0-7f9a-11eb-1fc5-c78f4f32650d
-# ╠═4db8cd20-7f9a-11eb-21d2-f91970955fb9
-# ╠═b4e7d1a0-7f98-11eb-167c-49f9786f6d56
-# ╠═03baf500-7f99-11eb-2c5f-051c7cb1d6bd
-# ╠═03bb4322-7f99-11eb-044b-dd69e68ab1b1
-# ╠═03bbb850-7f99-11eb-3407-812291e20300
-# ╠═03c46ae0-7f99-11eb-1e1b-07a8e974a677
-# ╠═618a75c0-7f99-11eb-152d-b9425d936d94
-# ╠═618fccee-7f99-11eb-3a5a-099361bc502a
-# ╠═6197e340-7f99-11eb-2d50-4f4830647f10
-# ╠═619fab70-7f99-11eb-20ba-a18c27145101
-# ╠═61a79ab0-7f99-11eb-0384-855140c82865
-# ╠═61b0c270-7f99-11eb-3be1-b7cdf8819f78
-# ╠═61b9c320-7f99-11eb-0ab4-810f4d4ba951
-# ╠═61c2eae0-7f99-11eb-20d5-5d9d94a6bbe2
+# ╠═c61b7d78-7fa2-11eb-3317-27c52adcacd6
+# ╠═d8e66710-7fa2-11eb-108a-9b4940798246
+# ╠═03c41ab0-7f9e-11eb-2b3e-435a5db42212
+# ╠═dfc383ca-7f9c-11eb-1cae-85de56d9cfb3
+# ╠═e9f7f20e-7f9c-11eb-3ab2-37fa0ecc4d97
+# ╠═a3bcff66-7f9d-11eb-3264-91ba931fd331
+# ╠═4aa60eb0-7f9d-11eb-24c7-bd6a81c79b66
+# ╠═acdd3c10-7f9e-11eb-15a1-fd2ee4f552c4
+# ╠═be4e263a-7f9e-11eb-3ac1-e791d0b6f87e
+# ╠═8effea2c-7f9d-11eb-0434-99749285b842
+# ╠═9237e558-7f9f-11eb-3fd2-9be5c7ebf90d
+# ╠═1b395122-7fa3-11eb-23bb-cf9a29cd27d2
+# ╠═96a2f21c-7fa0-11eb-3ae8-8563b736b30e
+# ╠═c3ca09d8-7fa0-11eb-17f8-cf753ad27443
+# ╠═642873f2-7fa2-11eb-0756-55cbf93d273d
+# ╠═7ddc0ba6-7fa2-11eb-1133-c91b4245a1eb
+# ╠═a1cf8518-7fa2-11eb-1783-df1ff91cda12
